029988ba78453985bc63153e7078b8fd99601c28
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-16 19:04:40 +0200

    feat(shadcn): installs shadcn button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@shadcn-ui/components/ui/button";
 import "./index.css";
 
 export const App = () => {
-  return <h1 className="text-xl">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-xl">Hello World</h1>
+      <Button onClick={() => console.log("Hello World")}>Click Me</Button>
+    </>
+  );
 };
