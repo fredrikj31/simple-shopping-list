d914a27bf1b031b03daadb6657f286d933e5d1c2
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-20 09:59:13 +0200

    feat(database): adds initial list database structure & sample query

---
+++ src/database/list/queries/getLists.ts
@@ -0,0 +1,8 @@
+import { IDBPDatabase } from "idb";
+import { ShoppingListDB } from "../..";
+import { STORE_NAME } from "..";
+
+export const getLists = async (db: IDBPDatabase<ShoppingListDB>) => {
+  const lists = db.getAll(STORE_NAME);
+  return lists;
+};
