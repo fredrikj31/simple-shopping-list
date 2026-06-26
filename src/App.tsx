8d2834a7461f7733041bee3f7336ad979b178b4c
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-26 22:21:34 +0200

    feat(app): adds @tanstack/react-router provider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HomeRoute } from "./routes/home/route";
 import { SettingsRoute } from "./routes/settings/route";
 import { PWAInstallPrompt } from "./components/PWAInstallPrompt";
@@ -7,14 +8,18 @@ import { PWAUpdatePrompt } from "./components/PWAUpdatePrompt";
 import { Toaster } from "@shadcn-ui/components/ui/sonner";
 
 export const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeRoute />} />
-          <Route path="/settings" element={<SettingsRoute />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomeRoute />} />
+            <Route path="/settings" element={<SettingsRoute />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
       <PWAInstallPrompt />
       <PWAUpdatePrompt />
       <Toaster />
