476c5747e341c8a182ad20b6a737ed9278e67ae2
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-26 20:34:10 +0200

    fix(database): adds missing await to getLists database query

--- src/database/list/queries/getLists.ts
+++ src/database/list/queries/getLists.ts
@@ -3,6 +3,6 @@ import { ShoppingListDB } from "../..";
 import { STORE_NAME } from "..";
 
 export const getLists = async (db: IDBPDatabase<ShoppingListDB>) => {
-  const lists = db.getAll(STORE_NAME);
+  const lists = await db.getAll(STORE_NAME);
   return lists;
 };
