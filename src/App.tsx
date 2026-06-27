0dec473fcf45ba7f0ce917bebfec76d6ebc48517
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-27 09:32:24 +0200

    feat(app): adds List route to routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HomeRoute } from "./routes/home/route";
-import { SettingsRoute } from "./routes/settings/route";
+import { Toaster } from "@shadcn-ui/components/ui/sonner";
 import { PWAInstallPrompt } from "./components/PWAInstallPrompt";
 import { PWAUpdatePrompt } from "./components/PWAUpdatePrompt";
-import { Toaster } from "@shadcn-ui/components/ui/sonner";
+import { HomeRoute } from "./routes/home/route";
+import { SettingsRoute } from "./routes/settings/route";
+import { ListRoute } from "./routes/list/route";
 
 export const App = () => {
   const queryClient = new QueryClient();
@@ -17,6 +18,9 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<HomeRoute />} />
             <Route path="/settings" element={<SettingsRoute />} />
+            <Route path="/list">
+              <Route path=":id" element={<ListRoute />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
