05effe8a87e3eac8ed4b5205fb024705017ba734
Fredrik Johansen <fredrik1206@gmail.com>
2026-07-07 21:36:52 +0200

    fix(database): fixes list database "by-list-id" index type

--- src/database/item/index.ts
+++ src/database/item/index.ts
@@ -6,7 +6,7 @@ export const STORE_NAME = "items" as const;
 export interface ItemStore {
   key: string;
   value: { id: string; listId: string; text: string; completed: boolean };
-  indexes: { "by-list-id": number };
+  indexes: { "by-list-id": string };
 }
 
 export const itemDatabase = {
