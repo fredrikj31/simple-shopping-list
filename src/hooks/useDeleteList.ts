59432cbc6efabf1c24206b9d37f2e64aa039af4f
Fredrik Johansen <fredrik1206@gmail.com>
2026-07-08 13:53:57 +0200

    feat(hooks): makes use of API deleteList call instead of database query

--- src/hooks/useDeleteList.ts
+++ src/hooks/useDeleteList.ts
@@ -1,14 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { listDatabase } from "../database/list";
+import { deleteList } from "../api/deleteList";
 import { queryKeys } from "../constants/queryKeys";
 
 export const useDeleteList = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: listDatabase.deleteList,
-    onSuccess: () => {
+    mutationFn: deleteList,
+    onSuccess: (_data, { listId }) => {
       queryClient.invalidateQueries({ queryKey: queryKeys.lists.list });
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.items.getItemsByListId(listId),
+      });
     },
   });
 };
