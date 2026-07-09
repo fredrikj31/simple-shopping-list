6e840162f31976d5a557454d7f738243eb67ab14
Fredrik Johansen <fredrik1206@gmail.com>
2026-07-09 08:32:35 +0200

    fix(hooks): invalidate list items on item creation

--- src/hooks/item/useCreateItem.ts
+++ src/hooks/item/useCreateItem.ts
@@ -1,8 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { itemDatabase } from "../../database/item";
+import { queryKeys } from "../../constants/queryKeys";
 
 export const useCreateItem = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: itemDatabase.createItem,
+    onSuccess: (_, { listId }) => {
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.items.getItemsByListId(listId),
+      });
+    },
   });
 };
